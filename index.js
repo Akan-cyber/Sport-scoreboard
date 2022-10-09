f8f7dabfa7d12e843370d341fdc019f2eb3404ea
Akan <82811999+Akan-cyber@users.noreply.github.com>
2022-10-09 23:14:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+let homeStoreEl =document.getElementById("text")
+let homeScoreEl =document.getElementById("textt")
+let text = 0
+let textt = 0
+
+function increaseHomeScoreOne(){
+    text += 1
+    homeStoreEl.textContent = text
+}
+function increaseHomeScoreTwo(){
+    text += 2
+    homeStoreEl.textContent = text
+}
+function increaseHomeScoreThree(){
+    text += 3
+    homeStoreEl.textContent = text
+}
+
+function increaseHomeScore1(){
+    textt += 1
+    homeScoreEl.textContent = textt
+}
+function increaseHomeScore2(){
+    textt += 2
+    homeScoreEl.textContent = textt
+}
+function increaseHomeScore3(){
+    textt += 3
+    homeScoreEl.textContent = textt
+}
+
+
